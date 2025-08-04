ead742c50fc19d16f316d94a0e0164d505089ad8
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-08-04 12:44:23 +0100

    feat: Add javascript functions to do operations

---
+++ scripts/operations.js
@@ -0,0 +1,40 @@
+/*
+File containing code to carry out 
+each of the mathematical operations possible 
+in the calculator.
+*/
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+        default:
+            return null;
+    }
+}
+
+let firstNum;
+let secondNum;
+let operator;
