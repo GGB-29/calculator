39185d7aae111f9c6c10cfc23754bfaca8cf1844
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-08-05 18:53:40 +0100

    fix: Fix bug where numbers could overflow display.

--- scripts/dom.js
+++ scripts/dom.js
@@ -3,9 +3,14 @@ File containing code to modify the DOM
 during use of the calculator.
 */
 
-
+function isValidLength() {
+    let displayContents = document.querySelector('#display').textContent.split('');
+    let displayLength = displayContents.length;
+    return (displayLength < 10);
+}
 
 function addCharacter(char) {
+    if (!isValidLength()) {return;};
     let display = document.querySelector('#display');
     display.textContent += char;
 }
